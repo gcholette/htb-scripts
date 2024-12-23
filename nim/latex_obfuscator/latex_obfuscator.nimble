5657b0df293e343fcd247b6456b5a36f19959fab
gcholette <solutions@gcholette.com>
2024-12-23 00:10:42 -0500

    chore: trying stuff with nim docs

--- nim/latex_obfuscator/latex_obfuscator.nimble
+++ nim/latex_obfuscator/latex_obfuscator.nimble
@@ -1,13 +1,13 @@
 # Package
-
-version       = "0.1.0"
+version       = "0.1.1"
 author        = "gcholette"
 description   = "Basic latex obfuscation"
 license       = "MIT"
 srcDir        = "src"
 bin           = @["latex_obfuscator"]
 
-
 # Dependencies
-
 requires "nim >= 2.2.0"
+
+task docs, "Generates documentation":
+  exec "nim doc --project --index:on --outdir:htmldocs ./src/**.nim"
---
+++ nim/latex_obfuscator/src/latex_obfuscator.nim
@@ -0,0 +1,34 @@
+from "std/cmdline" import paramStr, paramCount
+from strformat import fmt 
+from strutils import join, split
+from sequtils import mapIt
+from sugar import collect
+
+proc obfuscateLatexFromFile (sourceFilePath: string): string =
+  ## Reads a LaTeX file, obfuscates it by converting every character 
+  ## to its hexadecimal representation, and outputs the result.
+  ## Useful for bypassing filters.
+  ## 
+  ## For instance, \write18 becomes ^^5C^^77^^72^^69^^74^^65^^31^^38,
+  ## this is completely valid LaTeX.
+
+  let sourceFile = open(sourceFilePath)
+  let obfuscatedLatex = collect:
+    for line in sourceFile.lines:
+      if line.len != 0:
+        "^^" & join(line.mapIt(fmt"{ord(it):X}"), "^^")
+      else: ""
+
+  sourceFile.close()
+  join(obfuscatedLatex, "\n")
+
+when isMainModule:
+  if paramCount() < 1:
+    echo "Usage: <source-latex-file>"
+    quit(1)
+
+  let fileParam = paramStr(1)
+
+  echo obfuscateLatexFromFile fileParam
+
+export obfuscateLatexFromFile
