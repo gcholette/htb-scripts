fae7bde3ba9b8c5893cb1255fb44bd564b09394d
gcholette <solutions@gcholette.com>
2025-01-01 23:58:42 -0500

    tests: remove deprecated tests

--- nim/boxscanner/tests/tfnutils.nim
+++ nim/boxscanner/tests/tfnutils.nim
@@ -20,20 +20,3 @@ suite "uFold":
         acc[curr] = curr + 1,
       initOrderedTable[int, int]()
     ) == { 1: 2, 2: 3, 3: 4 }.toOrderedTable
-
-suite "Infix operators":
-  test "|> works as expected with single arg functions":
-    proc a(x: int): int = x + 1
-    proc b(x: int): string = &"-{x}-"
-    check 3 |> a == 4
-    check 3 |> a |> b == "-4-"
-    check 3 |> a |> b == 3.a.b
-
-  test "|> works as expected with multi arg functions":
-    proc a(x: int, y: int): int = x + y + 1
-    proc b(x: int): int = x * 2 
-    proc c(x: int, y:string, z: string, w: string): string = &"{y}{z}{w}{x}"
-    check 0 |> c("**", "_", "~") == "**_~0"
-    check 3 |> a(3) |> b |> c("**", "_", "~") == "**_~14"
-    check 2 |> a(1) |> b |> c("**", "_", "~") == 2.a(1).b.c("**", "_", "~")
-  
